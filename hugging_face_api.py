d30e08d55e8e8248d944c1efd6fd203488369d1c
veepsrock <veepsrock>
2023-11-01 20:06:36 +0000

    Getting model downloads number and seeing some eda

--- hugging_face_api.py
+++ hugging_face_api.py
@@ -7,6 +7,9 @@
 
 from huggingface_hub import HfApi, list_models
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
 
 # COMMAND ----------
 
@@ -37,6 +40,7 @@ for model in models:
     m["author"] = model.author
     m["config"] = model.config
     m["securityStatus"] = model.securityStatus
+    m["downloads"] = model.downloads
     model_list.append(m)
 
 # COMMAND ----------
@@ -45,11 +49,59 @@ df = pd.DataFrame(model_list)
 
 # COMMAND ----------
 
-df.shape
+df["author"].unique()
 
 # COMMAND ----------
 
 df.head()
+
+# COMMAND ----------
+
+df["author"].unique()
+
+# COMMAND ----------
+
+df.head()
+
+# COMMAND ----------
+
+df['user']= df['modelId'].apply(lambda x: x.split('/')[0] if x.find('/')!=-1 else "None" )
+
+
+# COMMAND ----------
+
+df['modelName']= df['modelId'].apply(lambda x: x.split('/')[1] if x.find('/')!=-1 else x )
+
+# COMMAND ----------
+
+# Sorting the DataFrame by downloads and selecting the top 20 rows
+df_sorted = df.sort_values('downloads', ascending=False).head(20)
+
+# Creating a bar chart for the top 20 downloads
+plt.figure(figsize=(10, 6))
+plt.barh(df_sorted['modelName'], df_sorted['downloads'], color='skyblue', edgecolor='black')
+plt.title('Top 20 Downloads by Model Name')
+plt.xlabel('Model Name')
+plt.ylabel('Downloads')
+
+# Customizing the x-axis labels to display values in millions
+def format_func(value, tick_number):
+    if value >= 1e6:
+        value = value / 1e6
+        return f'{value:.1f}M'
+    else:
+        return value
+
+plt.gca().xaxis.set_major_formatter(FuncFormatter(format_func))
+plt.show()
+
+# COMMAND ----------
+
+df_sorted
+
+# COMMAND ----------
+
+df["modelId"].str.split("/", expand = True)
 
 # COMMAND ----------
 
